06e5e89e8b18eb79331d639c1eb6ebc86e115d54
lichengkun <lichengkun@csrd.cn>
2023-12-06 11:17:30 +0800

    添加组件自动导入配置

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import routes from '~pages';
 
-console.log('目前路由表：', routes);
-
 export default createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes,
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Pages from 'vite-plugin-pages'; //See: https://github.com/hannoeru/vite-plugin-pages
 import AutoImport from 'unplugin-auto-import/vite'; //See: https://github.com/unplugin/unplugin-auto-import
+import Components from 'unplugin-vue-components/vite'; //See: https://github.com/unplugin/unplugin-vue-components
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,9 @@ export default defineConfig({
 		vue(),
 		AutoImport({
 			imports: ['vue', 'vue-router', 'pinia'],
+			dts: true,
+		}),
+		Components({
 			dts: true,
 		}),
 		Pages(),
